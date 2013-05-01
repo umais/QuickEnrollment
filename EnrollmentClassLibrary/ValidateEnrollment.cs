0213f4a7d66ca88e6a4d38bfb64606dcea9bf45d
iambillmccann <bill.mccann@gmail.com>
2013-04-30 20:31:28 -0400

    Refactor validation checks
    Deleted extra transaction classes to keep the code simpler while we are
    working out the design patterns.

---
+++ EnrollmentClassLibrary/ValidateEnrollment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EnrollmentClassLibrary
+{
+    /// <summary>
+    /// The <c>ValidateEnrollment</c> class edit the elements of the enrollment transaction to assure they
+    /// can be accepted by CMS.
+    /// </summary>
+    class ValidateEnrollment
+    {
+        
+    }
+}
